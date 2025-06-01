98fbcf8f8d8ff65d3878fff2fca38298d40a1457
Collins <upendocollins12@gmail.com>
2025-06-01 17:08:06 +0300

    add test dashboard script

---
+++ dashboard.py
@@ -0,0 +1,6 @@
+# streamlit run app.py --server.address=0.0.0.0 --server.port=8501
+import streamlit as st
+
+st.title("🚀 Minimal Streamlit Dashboard")
+st.write("Hello, world!")
+st.line_chart([1, 3, 2, 4])
